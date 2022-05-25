62a4f76cd7592c91a5d22d595247cc5c5e8948b1
Nick Garrone <nkj9hk@virginia.edu>
2022-05-25 11:06:53 -0400

    fetch and display data

--- our-app/src/components/ClassesTable.js
+++ our-app/src/components/ClassesTable.js
@@ -1,4 +1,5 @@
 import db from "../firebase"
+import {collection, getDocs} from "firebase/firestore";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -7,11 +8,27 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Button } from "@mui/material";
+import { useEffect, useState } from "react";
 
 const ClassesTable = (props) =>{
     const exampleData = [{teacher: "Smith", numStudents: 21}, {teacher: "Jones", numStudents: 32}, {teacher: "Spjziak", numStudents: 19},]
+    const [classesData, updateClassesData] = useState([]);
+
+    const fetchClassesData = () =>{
+        let classesArray = [];
+        getDocs(collection(db, "classes"))
+        .then((allDocs) => {allDocs.forEach((doc) => 
+        classesArray.push({teacher : doc.data().teacher, "numStudents": doc.data()["numberstudents"]})
+        )})
+        .then(()=>updateClassesData([...classesArray]));
+    }
+
+    useEffect(()=>{
+        fetchClassesData();
+    }, [])
+
     return(<>
-    <div class="table-container">
+    <div className="table-container">
     <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="classes table">
             <TableHead>
@@ -22,13 +39,13 @@ const ClassesTable = (props) =>{
             </TableRow>
             </TableHead>
             <TableBody>
-                {exampleData.map((entry)=>{
+                {classesData !== [] && classesData.map((entry)=>{
                     return(
                         <>
                         <TableRow
                         key={entry.teacher}
                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        ></TableRow>
+                        >
                         <TableCell component="th" scope="row" align="center">
                         {entry.teacher}
                         </TableCell>
@@ -36,6 +53,7 @@ const ClassesTable = (props) =>{
                         {entry.numStudents}
                         </TableCell>
                         <TableCell><Button variant="contained">View</Button></TableCell>
+                        </TableRow>
                         </>
                     )
                 })}
