6251cc44f329207690f189ac7b895f2c534d216c
Nick Garrone <nkj9hk@virginia.edu>
2022-05-25 15:31:17 -0400

    work on query

--- our-app/src/components/classesComp/Classes.js
+++ our-app/src/components/classesComp/Classes.js
@@ -3,6 +3,7 @@ import EditClasses from "./EditClasses";
 import "./classes.css"
 
 const Classes = (props) =>{
+
     return (<>
     <h1>Classes</h1>
     <EditClasses />
--- our-app/src/components/classesComp/IndividualClass.js
+++ our-app/src/components/classesComp/IndividualClass.js
@@ -1,13 +1,27 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import db from "../../firebase"
 
 const IndividualClass = (props) =>{
     let params = useParams();
+    const [classData, updateClassData] = useState([]);
+
+    const fetchClassData = () =>{
+        const classesRef = collection(db, "classes");
+        const q = query(classesRef, where('teacher', '==', params.teacherName.toString()));
+        getDocs(q)
+        .then((querySnapshot) => updateClassData(querySnapshot.data())) //only return first teacher if multiple results w/ name\
+        .then(()=>console.log(Object.assign({}, classData)));
+    }
+
+    useEffect(()=>{
+        fetchClassData();
+    }, [])
+
     return (
     <>
     <h1>{params.teacherName}'s Class</h1>
-    <span></span>
-    <br></br>
-    <h1>A</h1>
     </>
     )
 }
