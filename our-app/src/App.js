f0c306042aca8823ab570d32a5196cda053f28cb
Glendon Chin <glendonchin@gmail.com>
2022-05-25 13:50:19 -0400

    landing page initial draft

--- our-app/src/App.js
+++ our-app/src/App.js
@@ -1,4 +1,9 @@
-import './App.css';
+import React from 'react';
+import Header from './landing_page/landing_page_components/Header';
+import Functions from './landing_page/landing_page_components/Functions';
+import { makeStyles } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+
 import { Routes, Route, Link } from 'react-router-dom';
 import Classes from './components/classesComp/Classes';
 import Home from './components/Home';
@@ -6,13 +11,32 @@ import Error from './components/Error';
 import Calendar from './components/calenderComp/Calendar';
 import IndividualClass from './components/classesComp/IndividualClass';
 
+//insert home page background here
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    backgroundImage: `url(${process.env.PUBLIC_URL + ''})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <Header />
+      <Functions />
+    </div>
+
+    /*
     <div className="App">
       <nav>
         <Link to="/"> Home </Link>
         <Link to="/classes"> Classes </Link>
         <Link to="/calendar"> Calendar </Link>
+
       </nav>
 
       <Routes>
@@ -23,6 +47,8 @@ function App() {
         <Route path='/*' element={<Error/>}/>
       </Routes>
     </div>
+  );
+  */
   );
 }
 
