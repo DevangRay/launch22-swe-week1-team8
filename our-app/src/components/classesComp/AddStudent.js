259ac34c028e09435fbf636b55b0803929015f48
Nick Garrone <nkj9hk@virginia.edu>
2022-05-26 15:43:43 -0400

    Add students

--- our-app/src/components/classesComp/AddStudent.js
+++ our-app/src/components/classesComp/AddStudent.js
@@ -1,15 +1,35 @@
-import { Button, TextField } from "@mui/material"
+import { Button, Input } from "@mui/material"
+import { useParams, Link } from "react-router-dom"
+import { collection, addDoc } from "firebase/firestore";
+import { useRef } from "react";
+import db from "../../firebase";
 
 const AddStudent = (props) =>{
+    let params = useParams();
+    let nameRef = useRef();
+    let gradeRef = useRef();
+
+    const sendStudent = () =>{
+        addDoc(collection(db, "classes"), 
+        {
+            "studentteacher" : params.teacherName,
+            "grade" : gradeRef.current.value ? gradeRef.current.value : 0,
+            "studentname" : nameRef.current.value ? nameRef.current.value : ""
+        })
+        .then()
+    }
+
     return (
         <>
         <h2>Add Student</h2>
-        <TextField label="Student Name"></TextField>
-        <TextField label="Grade" style={{marginLeft:"5px"}}></TextField>
+        <Input inputRef={nameRef} defaultValue="Student Name"></Input>
+        <Input style={{marginLeft:"5px"}} inputRef={gradeRef} defaultValue="Grade"></Input>
         <br></br>
-        <Button variant="contained" style={{ marginTop:"5px" }}>Submit</Button>
+        <Button variant="contained" style={{ marginTop:"5px"}} onClick={()=>sendStudent()}>Submit</Button>
         <br></br>
-        <Button variant="outlined" style={{ marginTop:"5px" }}>Back</Button>
+        <Button variant="outlined" style={{ marginTop:"5px"}}>
+            <Link to={"../classes/"+params.teacherName}>Back</Link>
+        </Button>
         </>
     )
 }
