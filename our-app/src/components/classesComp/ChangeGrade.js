542e46de6ec53d4d434a2d86ab82cf0b14cee52b
Nick Garrone <nkj9hk@virginia.edu>
2022-05-26 11:24:35 -0400

    update grade state

--- our-app/src/components/classesComp/ChangeGrade.js
+++ our-app/src/components/classesComp/ChangeGrade.js
@@ -1,14 +1,21 @@
 import { useParams } from "react-router-dom"
-import { Input } from "@mui/material";
-import { useState } from "react";
+import { Input, Button } from "@mui/material";
+import { useRef, useState } from "react";
 
 const ChangeGrade = () =>{
     let params = useParams();
+    let gradeRef = useRef();
+
     const [grade, updateGrade] = useState(params.grade);
+
+    const changeGrade = () =>{
+        updateGrade(gradeRef.current.value);
+    }
 
     return(<>
         <h1>Change {params.studentName}'s Grade</h1>
-        <Input value={grade}></Input>
+        <Input defaultValue={grade} variant="" inputRef={gradeRef}></Input>
+        <Button variant="contained" onClick={changeGrade}>Update</Button>
     </>)
 }
 
