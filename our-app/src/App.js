3f3a9f16f29fb2fcdc640399bb684c74f2a61c3a
Devang Ray <devangray624@gmail.com>
2022-05-26 11:59:22 -0400

    fixed link and moved nav bar

--- our-app/src/App.js
+++ our-app/src/App.js
@@ -1,25 +1,19 @@
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom';
 import Classes from './components/classesComp/Classes';
-import Home from './components/routerComp/Home';
 import Error from './components/routerComp/Error';
 import Calendar from './components/calenderComp/Calendar';
 import Directory from './components/directoryComp/Directory';
 import IndividualClass from './components/classesComp/IndividualClass';
 import ChangeGrade from './components/classesComp/ChangeGrade';
+import MainPage from './landing_page/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <nav>
-        <Link to="/"> Home </Link>
-        <Link to="/classes"> Classes </Link>
-        <Link to="/calendar"> Calendar </Link>
-        <Link to="/directory"> Directory </Link>
-      </nav>
 
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<MainPage/>}/>
         <Route path="/classes" element={<Classes/>} />
         <Route path="/classes/:teacherName" element={<IndividualClass />} />
         <Route path="/student/:studentName" element={<ChangeGrade/>} />
@@ -27,6 +21,13 @@ function App() {
         <Route path="/directory" element = {<Directory/>} />
         <Route path='/*' element={<Error/>}/>
       </Routes>
+
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/classes"> Classes </Link>
+        <Link to="/calendar"> Calendar </Link>
+        <Link to="/directory"> Directory </Link>
+      </nav>
     </div>
   );
 }
